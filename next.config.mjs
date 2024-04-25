4372b7adb3156b0dc049dd78e90549453f4891c1
Kenson Hui <kenson.hui22@gmail.com>
2024-04-24 20:54:46 -0400

    Fix base path asset url

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/website",
+  output: "export",
+  assetPrefix: process.env.ASSET_PREFIX,
+  basePath: process.env.BASE_PATH,
 };
 
 export default nextConfig;
